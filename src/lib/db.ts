513a4e09e7ef3ef5ca81b3958248053477942af1
amegally <amegally@gmail.com>
2025-04-23 21:14:34 -0500

    fix: Change let to const for prismaGlobal

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 // This is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
-let prismaGlobal = global as typeof global & {
+const prismaGlobal = global as typeof global & {
   prisma?: PrismaClient;
 };
 
